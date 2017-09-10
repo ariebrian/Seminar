bbf0c4fb1456b840f023faadecc76d572e9917c2
Arie Brian <ariebrian22@gmail.com>
2017-09-11 01:36:52 +0700

    login regist carousel in php

---
+++ application/views/carousel.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="">
+	<!-- Bootstrap Core CSS -->
+    <link href="css/themes/journal/journal.bootstrap.min.css" rel="stylesheet">
+    <link href="css/shop-homepage.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container">
+    <center><h1>Seminar Kita</h1></center>
+	<div class="row carousel-holder">
+                        <div class="col-md-12">
+                            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img class="slide-image" src="images/gambar1.jpg" alt="">
+                                    </div>
+                                    <div class="item">
+                                        <img class="slide-image" src="images/gambar2.jpg" alt="">
+                                    </div>
+                                </div>
+                                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                </a>
+                                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+			</div>
+			
+	<!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ application/views/log.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="">
+	<!-- Bootstrap Core CSS -->
+    <link href="css/themes/journal/journal.bootstrap.min.css" rel="stylesheet">
+    <link href="css/shop-homepage.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="fontawesome/css/font-awesome.min.css">
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4 ">
+			<h1>Log In to Seminar</h1>
+			<form action="" method="post"> 
+				<div class="form-group">
+					<h3>Username</h3>
+					<input type="text" name="username" id="user_name" placeholder="username">
+				</div>
+				<div class="form-group">
+					<h3>Password</h3>
+					<input type="password" name="password" id="password" placeholder="password">
+				</div>
+				<div class="form-group">
+				<div class="btn-group">
+					<button type="button" class="btn btn-primary" aria-expanded="false">Log In</button>
+				</div>
+				</div>
+				<a href="">Forgot Password?</a>
+			</form>
+		</div>
+		<div class="col-lg-4"></div>
+	</div>
+</div>
+<!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
